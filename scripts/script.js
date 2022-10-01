856149d33d4a70755d4146c7410588569565afd3
Victor WSG-Laptop <le.victor@hotmail.com>
2022-10-01 16:24:27 -0500

    adjusting form style CSS

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+const inputName = document.querySelector('#form__input__name');
+const inputEmail = document.querySelector('#form__input__email');
+const inputCountry = document.querySelector('#form__input__country');
+const inputPassword = document.querySelector('#form__input__password');
+const inputRetypePassword = document.querySelector('#form__input__retype-password');
