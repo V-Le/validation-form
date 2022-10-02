4d251b154e6f53a88af9224660078105ab62eac5
Victor WSG-Laptop <le.victor@hotmail.com>
2022-10-01 19:28:54 -0500

    creating initial variables

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,9 @@ const inputEmail = document.querySelector('#form__input__email');
 const inputCountry = document.querySelector('#form__input__country');
 const inputPassword = document.querySelector('#form__input__password');
 const inputRetypePassword = document.querySelector('#form__input__retype-password');
+
+inputName.addEventListener('input', () => {
+    inputName.setCustomValidity('');
+    inputName.checkValidity();
+});
+
