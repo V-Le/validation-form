2762229c32903ab32de8c06351e27705714cc4ee
Victor WSG-Laptop <le.victor@hotmail.com>
2022-10-03 13:28:57 -0500

    adding validation icon function

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,4 @@
 const inputName = document.querySelector('#form__input__name');
-const inputEmail = document.querySelector('#form__input__email');
-const inputCountry = document.querySelector('#form__input__country');
 const inputPassword = document.querySelector('#form__input__password');
 const inputRetypePassword = document.querySelector('#form__input__retype-password');
 
