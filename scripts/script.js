490b940b13f1ec34ab098fa1f0a5190924505a72
Victor WSG-Laptop <le.victor@hotmail.com>
2022-10-03 13:42:50 -0500

    adding validation icon function

--- scripts/script.js
+++ scripts/script.js
@@ -30,28 +30,13 @@ inputPassword.addEventListener('invalid', () => {
     };
 })
 
-//Checks if password matches
-inputRetypePassword.addEventListener('input', () => {
-    if (inputPassword.value === inputRetypePassword.value) {
-        inputRetypePassword.setCustomValidity('');
-        inputPassword.checkValidity();
-    } else {
-        inputRetypePassword.setCustomValidity('Passwords must match');
-    };
-});
-
-inputRetypePassword.addEventListener('invalid', () => {
-    if (inputRetypePassword.value === '') {
-        inputRetypePassword.setCustomValidity('Please re-type your password.');
-    };
-});
-
 //Shows user the password requirements
 const checkCharLength = document.querySelector('#password-req__icon__6char');
 const checkSpecial = document.querySelector('#password-req__icon__special');
 const checkUpper = document.querySelector('#password-req__icon__upper');
 const checkLower = document.querySelector('#password-req__icon__lower');
 const checkNumber = document.querySelector('#password-req__icon__number');
+const checkMatch = document.querySelector('#password-req__icon__match');
 
 inputPassword.addEventListener('input', () => {
     if (inputPassword.value.length >= 6) {
@@ -65,6 +50,16 @@ inputPassword.addEventListener('input', () => {
     validationIconCheck(checkLower, /[a-z]/g);
     validationIconCheck(checkSpecial, /[^\da-zA-Z0-9]/g);
     validationIconCheck(checkNumber, /[0-9]/g);
+    checkPasswordMatch();
+});
+
+//Checks if password matches
+inputRetypePassword.addEventListener('input', checkPasswordMatch);
+
+inputRetypePassword.addEventListener('invalid', () => {
+    if (inputRetypePassword.value === '') {
+        inputRetypePassword.setCustomValidity('Please re-type your password.');
+    };
 });
 
 function validationIconCheck(nodeIcon, regexpress) {
@@ -77,5 +72,18 @@ function validationIconCheck(nodeIcon, regexpress) {
         nodeIcon.classList.remove('fa-check');
         nodeIcon.classList.add('fa-xmark');
     }
+};
+
+function checkPasswordMatch() {
+    if ((inputPassword.value === inputRetypePassword.value) && (inputPassword.value.length > 0)) {
+        checkMatch.classList.remove('fa-xmark');
+        checkMatch.classList.add('fa-check');
+        inputRetypePassword.setCustomValidity('');
+        inputPassword.checkValidity();
+    } else {
+        checkMatch.classList.remove('fa-check');
+        checkMatch.classList.add('fa-xmark');
+        inputRetypePassword.setCustomValidity('Passwords must match');
+    };
 };
 
